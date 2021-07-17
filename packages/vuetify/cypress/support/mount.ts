88b351d5d32f6e6cbc80ddfc4b4e4cc2225e647f
nekosaur <albert@kaaman.nu>
2021-07-17 17:35:57 +0200

    feat(VExpansionPanels): update to v3

--- packages/vuetify/cypress/support/mount.ts
+++ packages/vuetify/cypress/support/mount.ts
@@ -1,13 +1,18 @@
 import { mount as cyMount } from '@cypress/vue'
 import { createVuetify } from '../../src/entry-bundler'
+import { mergeDeep } from '../../src/util'
 
 Cypress.Commands.add('mount', (component, options, vuetifyOptions) => {
   const vuetify = createVuetify(vuetifyOptions)
   const defaultOptions = {
     global: {
       plugins: [vuetify],
+      stubs: {
+        transition: false,
+        'transition-group': false,
+      },
     },
   }
 
-  return cyMount(component, { ...defaultOptions, ...options })
+  return cyMount(component, mergeDeep(defaultOptions, options))
 })
